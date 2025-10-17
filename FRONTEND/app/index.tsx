b5887fa6262d9e800a90bd502f8b1eb64cc17e7b
Soha iqbal <suhaiqbal30@gmail.com>
2025-10-17 22:51:36 +0500

    added colors file

--- FRONTEND/app/index.tsx
+++ FRONTEND/app/index.tsx
@@ -4,11 +4,13 @@ import { Link } from "expo-router";
 import CustomButton from "@/components/CustomButton";
 import './globals.css'
 import Splash from "./splash";
+import colors from '../constants/colors'
 
 export default function Home() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Splash />
+      <Text style={{color:colors.secondary}}>Checking colors</Text>
       <Link href="/sign-in" asChild>
         <CustomButton title="Go to Sign" />
       </Link>
---
+++ FRONTEND/constants/colors.js
@@ -0,0 +1,17 @@
+export default {
+  primary: "#39A75A",
+  secondary: "#cc2929"
+
+
+
+
+
+
+
+
+//   secondary: '#6366F1',
+//   background: '#F9FAFB',
+//   text: '#111827',
+//   white: '#FFFFFF',
+//   gray: '#9CA3AF',
+};
